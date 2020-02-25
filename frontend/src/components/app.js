f5276fe36d1b38bd205b7fe60db579b39310298e
Neminem1203 <tpaul1203@gmail.com>
2020-02-25 14:54:33 -0500

    Debug more errors

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -5,11 +5,13 @@ import { Switch, Route } from 'react-router-dom';
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
+import Workout from "./workout/workout_container";
 
 const App = () => {
 	return (
 	<Switch>
 		<Route exact path="/" component={MainPage}/>
+		<Route exact path="/workout" component={Workout}/>
 		<AuthRoute exact path="/login" component={LoginFormContainer}/>
 		<AuthRoute eact path="/signup" component={SignupFormContainer}/>
 	</Switch>
