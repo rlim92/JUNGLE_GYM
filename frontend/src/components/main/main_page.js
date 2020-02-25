72c4a5cb95635e24b1ed3a6f17d4b1869b7f91d3
Robert <Robromano2@gmail.com>
2020-02-25 09:20:49 -0500

    Add errors reducer

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+	
+	render() {
+		return (
+			<div>
+				<h1>This is the main page</h1>
+			</div>
+		);
+	}
+}
+
+export default MainPage;
