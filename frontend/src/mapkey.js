44cb44709e8cbd35c1e7dc9e17d4809d73a5be37
Rich Lim <richardlim92@gmail.com>
2020-02-26 16:48:51 -0500

    Rename file

--- frontend/src/mapkey.js
+++ frontend/src/mapkey.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === "production") {
-    module.exports = require("./keys_prod");
+    module.exports = require("./mapkey_prod");
 } else {
-    module.exports = require("./keys_dev");
+    module.exports = require("./mapkey_dev");
 }
