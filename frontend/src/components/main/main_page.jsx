faae2397ca409808ada67fb13ae84ca07c768ed0
Kai <kaiyip.ho23@gmail.com>
2020-02-26 17:03:21 -0500

    Update main_page.jsx

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import NavBarContainer from '../nav/navbar';
 
 class MainPage extends React.Component {
 	render() {
 		return (
-			<div>
-				<h1>Welcome to the Jungle Gym</h1>
+			<div className="main-page-container">
+				<div className="main-nav-container">
+
+					<div>
+						<h1>Welcome to the Jungle Gym</h1>
+					</div>
+
+					<div className="nav-bar-container">
+						<NavBarContainer/>
+					</div>
+					
+				</div>
 			</div>
 		);
 	}
